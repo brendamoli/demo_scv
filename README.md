6d4660ff7ac4f13930cee4565005e47893613ed3
AdelaydaAlvarado <144193634+AdelaydaAlvarado@users.noreply.github.com>
2023-09-08 08:07:53 -0600

    Sudida del proyecto

---
+++ README.md
@@ -0,0 +1,9 @@
+# demo_scv
+
+proyecto_sistemacontrol_version
+
+proyecto  demo_scv
+
+brenda2023
+
+
---
+++ android/app/src/main/java/com/bimr/demo_scv/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bimr.demo_scv;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
